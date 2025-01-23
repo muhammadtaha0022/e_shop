b4cc3e639567d343a7f4803fad4362bc4112529a
muhammadtaha0022 <muhammadtaha00224443@gmail.com>
2025-01-23 22:33:36 +0500

    https://github.com/muhammadtaha0022/e_shop.git

--- Eshop/store/models/customer.py
+++ Eshop/store/models/customer.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Customer(models.Model):
-    first_name = models.CharField(max_length=50,default='Unknown',null=True, blank=True)
+    first_name = models.CharField(max_length=50,null=True, blank=True)
     last_name = models.CharField(max_length=50 ,null=True, blank=True)
     phone = models.CharField( max_length=15)
     email = models.EmailField(max_length=254)
@@ -11,3 +11,18 @@ class Customer(models.Model):
 
     def register(self):
         self.save()
+
+
+    def isexist(self):
+        # if Customer.objects.filter(email = self.email):
+        if Customer.objects.filter(email=self.email).exists():
+            return True
+    
+        return False
+    
+    @staticmethod
+    def get_customer_by_email(email):
+        try:
+            return Customer.objects.get(email=email)
+        except:
+            return False
--- Eshop/store/urls.py
+++ Eshop/store/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import index 
-from  . import views
+from .views import index , signup , login
+from  . import views 
 urlpatterns = [
-    path('',index),
-    path('signup/', views.signup, name = 'signup')
+    path('',index, name = 'homepage'),
+    path('signup/', views.signup, name = 'signup'),
+    # path('login/', views.login, name = 'login')
+    path('login/' , login.as_view())
 ]
